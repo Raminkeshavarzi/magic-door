ea4a5cd17675d6d7dc11ac2cc3bdfb40bd08acfe
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-14 12:27:17 +0200

    added the play door rule

--- JS/app.js
+++ JS/app.js
@@ -9,7 +9,7 @@ let doorImage3 = document.getElementById('door3');
 let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 let spacehDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
-
+let closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
 // Global variables
 let numClosedDoor = 3;
 let openDoor1 ;
@@ -18,13 +18,19 @@ let openDoor3 ;
 
 // Play door functions
 
+const isClicked = (door) =>{
+    if( door.src === closedDoorPath){
+        return false
+    } else {
+        return true
+    }
+}
 const playDoor = () => {
     numClosedDoor --;
     if(numClosedDoor === 0) {
         gameOver();
     }
 }
-
 
 
 // random generator
@@ -46,19 +52,27 @@ const randomDoorGenerator = () => {
 }
 
 doorImage1.onclick = () => {
-    doorImage1.src = openDoor1;
-    playDoor();
+    if (!isClicked(doorImage1)) {
+        doorImage1.src = openDoor1;
+        playDoor()
+    }
 }
 
 doorImage2.onclick = () => {
-    doorImage2.src = openDoor2;
-    playDoor();
+    if (!isClicked(doorImage2)) {
+        doorImage2.src = openDoor2;
+        playDoor()
+    }
 }
 
 doorImage3.onclick = () => {
-    doorImage3.src = openDoor3;
-    playDoor();
+    if (!isClicked(doorImage3)) {
+        doorImage3.src = openDoor3;
+        playDoor()
+    }
 }
+
+
 
 // invoking the functions
 randomDoorGenerator();
