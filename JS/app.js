701ee6820b6f640a122858d5dafaec559c7c4d1e
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-14 11:40:01 +0200

    added the img src for each photo and onclick method for doors

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,22 @@
 // Global variable
 let doorImage1 = document.getElementById('door1');
-let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg'
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
+
+// images src
+let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+let spacehDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+
+
 doorImage1.onclick = () => {
-doorImage1.src=botDoorPath
+doorImage1.src = botDoorPath
 }
 
+doorImage2.onclick = () =>{
+    doorImage2.src = spacehDoorPath
+}
+
+doorImage3.onclick = () => {
+    doorImage3.src = beachDoorPath
+}
